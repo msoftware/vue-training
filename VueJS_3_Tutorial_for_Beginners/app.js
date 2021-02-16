9430c87505f426cbccd898f0de8ef773c4dd2060
Michael Jentsch <jentsch.michael@gmail.com>
2021-02-16 16:18:51 +0100

    Show or hide condition with v-if test

--- VueJS_3_Tutorial_for_Beginners/app.js
+++ VueJS_3_Tutorial_for_Beginners/app.js
@@ -2,14 +2,17 @@ const app = Vue.createApp({
     data() {
         return {
             title: 'This is a title based on data',
-            body: 'This is a boring body.'
+            body: 'This is a boring body.',
+            showButtons: true
         }
     },
-    template: '<h2>{{ title }}</h2><p>{{ body }}</p><button @click="click($event, \'Hello\')">Click</button>',
     methods: {
         click(e, body) {
             console.log(e)
             this.body = body
+        },
+        toggleButtons() {
+            this.showButtons = !this.showButtons;
         }
     }
 })
